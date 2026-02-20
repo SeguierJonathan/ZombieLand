da10b372ecce605e565cfd79c071c37a0cfbb077
Jonathan Delporte <jonathan.delporte@oclock.school>
2026-02-20 14:24:38 +0100

    feat: implement express application

---
+++ app/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import "dotenv/config";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req,res) => {
+    res.send("hello world")
+});
+
+app.listen(PORT, () => {
+    console.log(`Serveur is running at http://localhost:${PORT}`)
+});
