0897a8b5e6f048b22beb444c28c603a07690bbd4
SeguierJonathan <jonathan.seguier@oclock.school>
2026-02-23 11:25:23 +0100

    feat: add comment in 01.create-tables.js

--- app/migrations/01.create-tables.js
+++ app/migrations/01.create-tables.js
@@ -1,5 +1,6 @@
 import { sequelize } from "../models/index.js";
 
+// Crée les tables suivant les models.
 await sequelize.sync({ force: true });
 console.log("✅ All models were synchronized successfully");
 
