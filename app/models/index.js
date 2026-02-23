ebacdad2bf98f1c3c816270cb554e3a33ab2c9af
SeguierJonathan <jonathan.seguier@oclock.school>
2026-02-23 12:33:39 +0100

    feat: add One-to-Many relation between Activity and Category

--- app/models/index.js
+++ app/models/index.js
@@ -2,6 +2,14 @@ import sequelize from "../config/sequelize-client.js";
 import Category from "./category.model.js";
 import Activity from "./Activity.model.js";
 
+Category.hasMany(Activity, {
+    foreignKey: 'categoryId',
+    as: 'activities'
+});
 
+Activity.belongsTo(Category, {
+    foreignKey: 'categoryId',
+    as: 'category'
+});
 
 export { sequelize, Category, Activity };
