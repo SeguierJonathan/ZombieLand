91774c572502534cd830e7562223b12500d31350
SeguierJonathan <jonathan.seguier@oclock.school>
2026-02-23 09:55:20 +0100

    feat: initialize Sequelize configuration

---
+++ app/config/sequelize-client.js
@@ -0,0 +1,19 @@
+import { Sequelize } from "sequelize";
+import "dotenv/config";
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    define: {
+        underscored: true,
+        timestamps: true,
+        freezeTableName: true
+    }
+});
+
+try {
+    sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+} catch (error) {
+    console.error('Unable to connect to the database:', error);
+}
+
+export default sequelize;
