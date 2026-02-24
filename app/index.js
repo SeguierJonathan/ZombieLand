4b805ca86f999cbb2686314c4a0730077517a758
SeguierJonathan <jonathan.seguier@oclock.school>
2026-02-24 16:40:11 +0100

    feat:add config express-session

--- app/index.js
+++ app/index.js
@@ -1,13 +1,26 @@
 import express from 'express';
 import "dotenv/config";
+import session from "express-session"
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req,res) => {
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "lax",
+        maxAge: 60 * 1000 //temps d'expiration en milliseconde
+    }
+}))
+
+app.get("/", (req, res) => {
     res.send("hello world")
 });
 
 app.listen(PORT, () => {
     console.log(`Serveur is running at http://localhost:${PORT}`)
-}) ;
+});
