248e29fb2124a97dc937a67996e7d2b0411b0d29
Jonathan Delporte <jonathan.delporte@oclock.school>
2026-03-02 18:51:53 +0100

    correction deleteUsers function

--- app/controllers/admin.users.controller.js
+++ app/controllers/admin.users.controller.js
@@ -8,12 +8,12 @@ export async function getAllUsers(req,res) {
 
 export async function deleteUsers(req, res) {
   const userId = req.params.id;
-  await User.destroy({
+  const result = await User.destroy({
     where: {
       id: userId,
     }
   });
-  if (userId === 0) {
+  if (!result) {
     notify.error(res, "Une erreur est survenue lors de la suppression de l'utilisateur");
     notify.redirect(res);
     res.redirect('/menu-administrateur/utilisateurs');
