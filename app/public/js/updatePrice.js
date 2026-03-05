b082f511fc7e939bd1897ba2504c1ebc77e7c39a
Jonathan Delporte <jonathan.delporte@oclock.school>
2026-03-05 14:41:57 +0100

    remove logs

--- app/public/js/updatePrice.js
+++ app/public/js/updatePrice.js
@@ -6,12 +6,8 @@ const totalPrice = document.getElementById("prix-total");
 console.log(input);
 
 input.addEventListener("input", (e) => {
-    console.log("input", input.value);
-    console.log("unitPrice", unitPrice.textContent);
     const nb_personne = parseInt(input.value);
-    console.log(typeof input.value);
     const price = parseInt(unitPrice.textContent);
     
-    totalPrice.textContent = (nb_personne * price) + " €";
-    
+    totalPrice.textContent = (nb_personne * price) + " €";  
 });
