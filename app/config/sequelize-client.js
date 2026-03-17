8567144da0da16dd5f2390e8984b4223fa83846f
SeguierJonathan <jonathan.seguier@oclock.school>
2026-03-17 13:05:22 +0100

    feat: split css

--- app/config/sequelize-client.js
+++ app/config/sequelize-client.js
@@ -10,7 +10,7 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
 });
 
 try {
-    sequelize.authenticate();
+    await sequelize.authenticate();
     console.log("Connection has been established successfully.");
 } catch (error) {
     console.error('Unable to connect to the database:', error);
