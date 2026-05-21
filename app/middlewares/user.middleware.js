bae3cb0ffc74ddf07ce660dbe273367af66c9b09
SeguierJonathan <jonathan.seguier@oclock.school>
2026-05-22 01:15:29 +0200

    feat: rework

--- app/middlewares/user.middleware.js
+++ app/middlewares/user.middleware.js
@@ -1,49 +1,27 @@
 import Joi from "joi";
 import { notify } from "../utils/common.js";
 
-export function validateAuthRegister(req, res, next) {
+export function validateUpdateAccount(req, res, next) {
 
     const schemaUser = Joi.object({
         firstName: Joi.string().trim().min(2).required(),
         lastName: Joi.string().trim().min(2).required(),
         email: Joi.string().trim().email().required(),
         password: Joi.string().trim().min(8).pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]).+$/).required(),
-        confirmPassword: Joi.string().valid(Joi.ref("password")).required()
+        newPassword: Joi.string().trim().min(8).empty("").pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]).+$/),
+        confirmPassword: Joi.string().empty("").when("newPassword", { is: Joi.exist(), then: Joi.valid(Joi.ref("newPassword")).required(), otherwise: Joi.optional() })
     })
 
     const validation = schemaUser.validate(req.body);
 
     if (validation.error) {
-        //!\\ revoir le retour en cas de mauvais données
-        return res.status(400).send(validation.error.details[0].message);
-    }
-    next();
+        console.log(validation.error);
 
-}
-
-export function validateAuthLogin(req, res, next) {
-
-    const schemaUser = Joi.object({
-        email: Joi.string().trim().email().required(),
-        password: Joi.string()
-            .trim().min(8)
-            .pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]).+$/)
-            .required(),
-    })
-
-    const validation = schemaUser.validate(req.body);
-
-    if (validation.error) {
         notify.error(res, 'Email ou mot de passe incorrect');
         // utiliser pour garder les notification apres un redirect
         notify.redirect(res);
         return res.redirect("/connexion");
     }
     next();
+
 }
-
-
-
-
-
-
